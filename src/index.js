33e7f5f319e5f25537d7a2fca22069a3b7d76172
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-07 13:40:19 -0700

    Integrate Typewriter effect on generated text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const generateContent = (e) => {
-  e.preventDefault();
+const generateContent = (event) => {
+  event.preventDefault();
   alert("Generating content...");
+
+  new Typewriter("#generated", {
+    strings:
+      "El corazón antañón viajará de regreso al ritmo ya olvidado de la vieja ilusión.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 };
 
 const form = document.getElementById("form");

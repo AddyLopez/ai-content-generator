2ddda7520d02421cf2c4f59811b29a2dd40a4be2
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-07 15:45:57 -0700

    Complete context of AI prompt engineering

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const generateContent = (event) => {
   // Prompt engineering
   const userInput = document.getElementById("text-input").value;
   let prompt = `Generate a poem in Spanish about ${userInput}`;
-  let context;
+  let context = `You are a literary AI assistant who returns an answer within an HTML <p> tag. Include line breaks using an HTML <br /> tag. Sign the poem with "SheCodes AI" on a new line within an HTML <strong> tag. If the poem includes the user's search term in Spanish, include the user's search term in an HTML <span> tag in the poem and add the CSS class "highlight" to that particular HTML <span> tag.`;
   const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(displayContent);

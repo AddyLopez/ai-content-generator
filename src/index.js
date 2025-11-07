b09af9594b542dc20857e5dd75d765acd56a738f
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-07 13:29:14 -0700

    Add event listener to form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const generateContent = (e) => {
+  e.preventDefault();
+  alert("Generating content...");
+};
 
+const form = document.getElementById("form");
+
+form.addEventListener("submit", generateContent);

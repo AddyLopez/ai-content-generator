05de521795f36038b54cd89054e41ba337c8d532
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-07 15:21:40 -0700

    Set up logic for API call to AI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const displayContent = () => {
+  alert("Content displayed!");
+};
+
 const generateContent = (event) => {
   event.preventDefault();
-  alert("Generating content...");
 
   new Typewriter("#generated", {
     strings:
@@ -9,6 +12,13 @@ const generateContent = (event) => {
     delay: 1,
     cursor: "",
   });
+
+  const apiKey = "085ca3tcfao1ca19876ded9ba834fb7c";
+  let prompt;
+  let context;
+  const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayContent);
 };
 
 const form = document.getElementById("form");

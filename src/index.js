bd03b59c6da47cdb8b2637ed2ac8c3128222004f
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-11 01:27:23 -0700

    Update context to improve UI

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const generateContent = (event) => {
   // Prompt engineering
   const userInput = document.getElementById("text-input").value;
   let prompt = `Generate a poem in Spanish about ${userInput}`;
-  let context = `You are a literary AI assistant who returns an answer within an HTML <p> tag. Include line breaks using an HTML <br /> tag. Sign the poem with "SheCodes AI" on a new line within an HTML <strong> tag. If the poem includes any instances of the user's search term in Spanish, always include the user's search term in an HTML <span> tag in the poem and add the CSS class "highlight" to that particular HTML <span> tag.`;
+  let context = `You are a literary AI assistant who returns an answer within an HTML <p> tag. Include line breaks using an HTML <br /> tag. Sign the poem with "SheCodes AI" on a new line within an HTML <strong> tag. If the poem includes any instances of the user's search term(s) in Spanish, always include the user's search term(s) in an HTML <span> tag in the poem and add the CSS class "highlight" to that particular HTML <span> tag.`;
   const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   new Typewriter("#generated", {

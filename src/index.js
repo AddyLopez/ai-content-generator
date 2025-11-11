7d9b481ff962542300104a2a06ceba07af2bf8cd
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-11 00:17:50 -0700

    Improve UX/UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const displayContent = (response) => {
   console.log("Displaying content...");
+
   new Typewriter("#generated", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 15,
     cursor: "",
   });
 };
@@ -16,8 +17,16 @@ const generateContent = (event) => {
   // Prompt engineering
   const userInput = document.getElementById("text-input").value;
   let prompt = `Generate a poem in Spanish about ${userInput}`;
-  let context = `You are a literary AI assistant who returns an answer within an HTML <p> tag. Include line breaks using an HTML <br /> tag. Sign the poem with "SheCodes AI" on a new line within an HTML <strong> tag. If the poem includes the user's search term in Spanish, include the user's search term in an HTML <span> tag in the poem and add the CSS class "highlight" to that particular HTML <span> tag.`;
+  let context = `You are a literary AI assistant who returns an answer within an HTML <p> tag. Include line breaks using an HTML <br /> tag. Sign the poem with "SheCodes AI" on a new line within an HTML <strong> tag. If the poem includes any instances of the user's search term in Spanish, always include the user's search term in an HTML <span> tag in the poem and add the CSS class "highlight" to that particular HTML <span> tag.`;
   const apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let generated = document.getElementById("generated");
+  new Typewriter("#generated", {
+    strings: `<p><em>Generating poem about <strong class="highlight">${userInput}</strong>...</em></p>`,
+    autoStart: true,
+    delay: 15,
+    cursor: "",
+  });
 
   axios.get(apiURL).then(displayContent);
 
